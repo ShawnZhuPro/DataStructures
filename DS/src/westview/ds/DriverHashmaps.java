39e5a7c1dd34fec4f8b848bc60007d515ddb5b4e
1905827 <1905827@01-19785.pusd.dom>
2024-03-26 11:36:30 -0700

    Add basic working code

--- DS/src/westview/ds/DriverHashmaps.java
+++ DS/src/westview/ds/DriverHashmaps.java
@@ -40,47 +40,32 @@ public class DriverHashmaps {
 		
 		// Get the scanner going to read the csv file
 		//which should be outside the src folder
-				
 		
-//		try {
-//			Scanner scanner = new Scanner(new File("covid417.csv"));
-//			
-//			while(scanner.hasNext()) {
-//				System.out.println(scanner.next());
-//			}
-//			
-//			scanner.close();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		
-//		HashMap states = new HashMap<String, ArrayList<String>>();
-//		HashMap map = new HashMap<String, states>();
-		
-		HashMap states = new HashMap<String, ArrayList<String>>();
+		// Data for everything with the city name as the key
 		HashMap map = new HashMap<String, HashMap<String, ArrayList<String>>>();
 
-
-		
 		try {
 			Scanner scanner = new Scanner(new File("covid417.csv"));
 			
 			while(scanner.hasNextLine()) {
+				// Data for the current state in the current line
+				HashMap states = new HashMap<String, ArrayList<String>>();
+
+				// Splits data for CSVs (using commas)
 				String[] row = scanner.nextLine().split(",");
 				String cityName = row[0];
 				String stateName = row[1];
 				ArrayList<String> list = new ArrayList<String>();
 				list.add(row[2]);  // confirmed
-				list.add(row[3]);  // deaths
-				list.add(row[4]);  // recovered
-				list.add(row[5]);  // active
+//				list.add(row[3]);  // deaths
+//				list.add(row[4]);  // recovered
+//				list.add(row[5]);  // active
+
 				states.put(stateName, list);
 				map.put(cityName, states);
 
 			}
-			System.out.println(map.get("Yuma").get(""));
 			scanner.close();
-			
 			
 			// User input ---------------------------------------------------
 			while(true) {
@@ -94,7 +79,7 @@ public class DriverHashmaps {
 				
 				String citySelected = userInput.nextLine();
 				System.out.println("The confirmed number of cases in " + citySelected + ", " +
-						stateSelected + " is : ");
+						stateSelected + " is : " + ((HashMap) map.get(citySelected)).get(stateSelected));
 				
 			}
 			// --------------------------------------------------------------
@@ -102,16 +87,7 @@ public class DriverHashmaps {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		
-		// If you want confirmed cases from Abbeville, South Carolina
-		int confirmed = 0;
-		int deaths = 1;
-		int recovered = 2;
-		int active = 3;
-//		String res = map.get("Abbeville");
-//		System.out.println(map.get("Abbeville"));
+
 
 	}
 
